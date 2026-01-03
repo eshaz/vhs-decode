7ec655f0362883ce3be04d0584b94dd96feaf25f
Ethan Halsall <ethanhalsall11@augustana.edu>
2026-01-03 17:00:31 -0600

    tune ranges

--- vhsdecode/hifi/HiFiCalibrate.py
+++ vhsdecode/hifi/HiFiCalibrate.py
@@ -464,15 +464,15 @@ def main() -> int:
         'expander_gain': {'min':DEFAULT_EXPANDER_GAIN,'max':DEFAULT_EXPANDER_GAIN,'step':1},
         'expander_ratio': {'min':DEFAULT_EXPANDER_RATIO,'max':DEFAULT_EXPANDER_RATIO,'step':1},
         
-        'expander_weighting_tau_1': {'min':0.000100,'max':0.000400,'step':0.000001},
+        'expander_weighting_tau_1': {'min':0.000150,'max':0.000300,'step':0.000001},
         'expander_weighting_tau_2': {'min':0.000010,'max':0.000100,'step':0.000001},
         'expander_weighting_db_per_octave': {'min':DEFAULT_VHS_EXPANDER_WEIGHTING_DB_PER_OCTAVE,'max':DEFAULT_VHS_EXPANDER_WEIGHTING_DB_PER_OCTAVE,'step':1},
-        'expander_weighting_bandwidth': {'min':1.5,'max':4.0,'step':0.02},
+        'expander_weighting_bandwidth': {'min':1.5,'max':3.5,'step':0.02},
         
-        'deemphasis_tau_1': {'min':0.000100,'max':0.000400,'step':0.000001},
+        'deemphasis_tau_1': {'min':0.000150,'max':0.000300,'step':0.000001},
         'deemphasis_tau_2': {'min':0.000010,'max':0.000100,'step':0.000001},
         'deemphasis_db_per_octave': {'min':DEFAULT_VHS_DEEMPHASIS_DB_PER_OCTAVE,'max':DEFAULT_VHS_DEEMPHASIS_DB_PER_OCTAVE,'step':1},
-        'deemphasis_bandwidth': {'min':1.5,'max':4.0,'step':0.02},
+        'deemphasis_bandwidth': {'min':1.5,'max':3.5,'step':0.02},
     }
     
     # Generate results lazily
