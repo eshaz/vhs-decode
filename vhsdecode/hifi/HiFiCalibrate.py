afd42ad80e8cb44bb407c355bbcbef102515bd63
Ethan Halsall <ethanhalsall11@augustana.edu>
2026-01-11 11:05:26 -0600

    tune parameters

--- vhsdecode/hifi/HiFiCalibrate.py
+++ vhsdecode/hifi/HiFiCalibrate.py
@@ -472,12 +472,12 @@ def main() -> int:
         
         'expander_weighting_tau_1': {'min':DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_1,'max':DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_1,'step':1},
         'expander_weighting_tau_2': {'min':DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_2,'max':DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_2,'step':1},
-        'expander_weighting_db_per_octave': {'min':18,'max':24,'step':0.1},
+        'expander_weighting_db_per_octave': {'min':20,'max':24,'step':0.1},
         'expander_weighting_bandwidth': {'min':2,'max':6,'step':0.1},
         
         'deemphasis_tau_1': {'min':DEFAULT_VHS_DEEMPHASIS_TAU_1,'max':DEFAULT_VHS_DEEMPHASIS_TAU_1,'step':1},
         'deemphasis_tau_2': {'min':DEFAULT_VHS_DEEMPHASIS_TAU_2,'max':DEFAULT_VHS_DEEMPHASIS_TAU_2,'step':1},
-        'deemphasis_db_per_octave': {'min':12,'max':18,'step':0.1},
+        'deemphasis_db_per_octave': {'min':15,'max':17,'step':0.1},
         'deemphasis_bandwidth': {'min':2,'max':3,'step':0.1},
     }
 
