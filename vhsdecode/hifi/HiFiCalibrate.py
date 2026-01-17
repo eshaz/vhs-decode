7a86894157f7af520220b9292bdd469868b19c6d
Ethan Halsall <ethanhalsall11@augustana.edu>
2026-01-17 15:09:04 -0600

    tune parameters

--- vhsdecode/hifi/HiFiCalibrate.py
+++ vhsdecode/hifi/HiFiCalibrate.py
@@ -496,7 +496,7 @@ def main() -> int:
     args = parser.parse_args()
 
     param_dict = {
-        'expander_attack_tau': {'min':0.001,'max':0.01,'step':1e-4},
+        'expander_attack_tau': {'min':0.001,'max':0.01,'step':1e-3},
         'expander_release_tau': {'min':DEFAULT_EXPANDER_RELEASE_TAU,'max':DEFAULT_EXPANDER_RELEASE_TAU,'step':1e-4},
         'expander_gain': {'min':DEFAULT_EXPANDER_GAIN,'max':DEFAULT_EXPANDER_GAIN,'step':1},
         'expander_ratio': {'min':DEFAULT_EXPANDER_RATIO,'max':DEFAULT_EXPANDER_RATIO,'step':1},
@@ -504,8 +504,8 @@ def main() -> int:
         'expander_weighting_tau_1': {'min':DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_1,'max':DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_1,'step':1e-7},
         'expander_weighting_tau_2': {'min':DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_2,'max':DEFAULT_VHS_EXPANDER_WEIGHTING_TAU_2,'step':1e-6},
         'expander_weighting_bandwidth': {'min':DEFAULT_VHS_EXPANDER_WEIGHTING_BANDWIDTH,'max':DEFAULT_VHS_EXPANDER_WEIGHTING_BANDWIDTH,'step':0.01},
-        'expander_weighting_low_pass': {'min':5000,'max':20000,'step':500},
-        'expander_weighting_low_pass_transition': {'min':5000,'max':200000,'step':200},
+        'expander_weighting_low_pass': {'min':6000,'max':20000,'step':1000},
+        'expander_weighting_low_pass_transition': {'min':5000,'max':200000,'step':1000},
         
         'deemphasis_tau_1': {'min':DEFAULT_VHS_NR_DEEMPHASIS_TAU_1,'max':DEFAULT_VHS_NR_DEEMPHASIS_TAU_1,'step':1},
         'deemphasis_tau_2': {'min':DEFAULT_VHS_NR_DEEMPHASIS_TAU_2,'max':DEFAULT_VHS_NR_DEEMPHASIS_TAU_2,'step':1},
